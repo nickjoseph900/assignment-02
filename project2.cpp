97b2c0224d3b73a50bdc5238c5437a3b17def91f
nickjoseph900 <njoseph1444@gmail.com>
2016-02-27 23:21:14 -0800

    Added files via upload

---
+++ project2.cpp
@@ -0,0 +1,24 @@
+//Nicholas Joseph
+#include <iostream>
+#include <string>
+#include "buffer.h"
+
+using namespace std;
+class Buffer {
+
+private:
+	char * data_;
+
+	array A[] = {}
+
+	data_[y * size_x + x];
+ 
+public:
+        const unsigned int size_x;
+		const unsigned int size_y;
+
+
+		Buffer(unsigned int size_x, unsigned int size_y);
+
+};
+
